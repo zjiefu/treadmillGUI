f151f26b46ab23e86156d1c17021cb8672455179
Jiefu Zhang <zjiefu@umich.edu>
2023-11-16 18:48:08 -0500

    Fix treadmill speed adjustment bug

--- callback.py
+++ callback.py
@@ -51,8 +51,9 @@ def speedIncrease(bertecObj):
     time.sleep(0.5)
 
     speedL, speedR = bertecObj.get_belt_speed()         # TODO: The speed of bertec is not a constant
-    speedL = speedL + 0.1
-    speedR = speedR + 0.1
+    speedAvg = (speedL + speedR) / 2
+    speedL = speedAvg + 0.1
+    speedR = speedAvg + 0.1
     bertecObj._write_command(speedL, speedR) 
 
     print("Speed Increased, now: ", speedL, "m/s", end='\r')
@@ -63,8 +64,9 @@ def speedDecrease(bertecObj):
     winsound.Beep(freq, dur)        # Just a beep sound to indicate the treadmill will slowdown
     time.sleep(0.5)
     speedL, speedR = bertecObj.get_belt_speed()
-    speedL = speedL - 0.1
-    speedR = speedR - 0.1
+    speedAvg = (speedL + speedR) / 2
+    speedL = speedAvg - 0.1
+    speedR = speedAvg - 0.1
     bertecObj._write_command(speedL, speedR) 
 
     print("Speed Decreased, now: ", speedL, "m/s", end='\r')
